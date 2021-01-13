9fde5cd812bc464ff386afbfb1c746c1e8d3bb07
Motyak <anfossotommy@hotmail.fr>
2021-01-13 12:16:08 +0100

    fixed euros

--- transpiler/s.sh
+++ transpiler/s.sh
@@ -8,6 +8,6 @@ fi
 # substitute prices with currency in front
 perl -pe 's/\$([0-9]+(?:\.[0-9]{1,2})?)/Prix{'\''\$'\'', $1}/g' $SRC |
 # substitute prices with currency in back
-perl -pe 's/([0-9]+(?:\.[0-9]{1,2})?)€/Prix{'\''\€'\'', $1}/g' |
+perl -pe 's/([0-9]+(?:\.[0-9]{1,2})?)€/Prix{L'\''€'\'', $1}/g' |
 # substitute percentages
 perl -pe 's/([0-9]+(?:\.[0-9]{1,2})?)%/Pourcentage{$1}/g'
