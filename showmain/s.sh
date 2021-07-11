88137b75c622ae5dbad0138c0d5a50c6556192ac
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-07-11 18:08:55 +0200

    exo1 OK

--- showmain/s.sh
+++ showmain/s.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SRC=$1
-if [ -z $SRC ] && [ $(ls -l *.cpp 2>/dev/null | wc -l) = "1" ]
-then
-	SRC="$(ls *.cpp)"
-fi
-grep -zoP 'int main\(.*\)(?:.|\n)*{((?:.|\n)*)}' $SRC
-echo ''
+
+[ -z $SRC ] &&
+[ $(ls -l *.cpp 2>/dev/null | wc -l) = "1" ] &&
+SRC="$(ls *.cpp)"
+
+grep -zoP 'int main\(.*\)(?:.|\n)*{((?:.|\n)*)}\n?' $SRC
