f373c8e042db970f85170f093b7f5af07a921668
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-04-16 02:09:38 +0200

    xor now takes a filename as arg instead of the actual key value

--- xor/xor.pl
+++ xor/xor.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/env perl
 
 my $input = join("", <STDIN>);
-my $key = $ARGV[0];
+my $key_file = $ARGV[0];
+open my $fh, '<:encoding(UTF-8)', $key_file or die "Can't open file, $!";
+read $fh, my $key, -s $fh;
 
 print($input ^ $key);
