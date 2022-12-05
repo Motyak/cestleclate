f759545d9aa093a222336be1176fbdbb508865d1
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-12-05 01:10:50 +0100

    added readonly map

---
+++ cppkeywords/utils/classes/ReadonlyMap.h
@@ -0,0 +1,19 @@
+#ifndef READONLY_MAP_H
+#define READONLY_MAP_H
+
+template <typename K, typename V>
+class ReadonlyMap {
+    std::map<K,V> map;
+  public:
+    ReadonlyMap(std::initializer_list<std::pair<K,V>> keyValuePairs) {
+        for (auto pair : keyValuePairs) {
+            this->map[pair.first] = pair.second;
+        }
+    }
+    
+    V operator[](K key) const {
+        return this->map.at(key);
+    }
+};
+
+#endif
