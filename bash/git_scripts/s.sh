8e8694158f1d543b0afd546619485aaa58e69c3a
Motyak <m0tyak_@live.fr>
2023-07-08 18:25:00 +0200

    added git_scripts

---
+++ bash/git_scripts/s.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+trap clean_and_exit EXIT
+function clean_and_exit {
+    local exit_code=$?
+
+	set +o errexit; trap - ERR # cleaning shall not fail
+	>&2 echo "Cleaning..."
+    git_unlock
+
+    exit $exit_code
+}
+
+g_locking=false
+
+function git_lock {
+	$g_locking && return 0 # git is already being locked => early return
+	# try and acquire the lock
+	until (set -o noclobber; > .git/index.lock) &>/dev/null; do
+		sleep 1 # retry in a second
+	done
+    g_locking=true
+}
+
+function git_unlock {
+    $g_locking || return 0 # git isnt being locked => early return
+    rm -f .git/index.lock
+    g_locking=false
+}
+
+git_lock # BEGIN TRANSACTION BLOCK
+	current_branch_name=$(git branch --show-current)
+	git fetch origin "$current_branch_name"
+	git merge --ff-only "origin/$current_branch_name" || {
+		# make a local backup of current branch #
+		backup_branch_name="backupp/$current_branch_name/$(date +%Y-%m-%d_%H-%M-%S)"
+		git branch "$backup_branch_name"
+	
+		# overwrite local branch with remote state
+		git reset --hard "origin/$current_branch_name"
+	}
+git_unlock # END TRANSACTION BLOCK
